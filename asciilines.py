6fea39f33c73e288be53965e2bf0a261592560a5
smilinhawaiian <shepherd.sharice@gmail.com>
2019-07-02 04:27:26 -0700

    added main program file asciilines.py

---
+++ asciilines.py
@@ -0,0 +1,16 @@
+# Copyright @ Sharice Mayer
+# [This program is licensed under the "MIT License"]
+# Please see the file LICENSE in the source
+# distribution of this software for license terms.
+
+# Course: CS461P - HW1 MiniProject
+# Date: 07/01/19
+
+# Reads a file in TVG format, and renders
+# the file as standard ascii output
+
+
+
+
+
+
