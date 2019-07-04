668410ac8d2c15c1302a715fcbbff7b8d476ab06
smilinhawaiian <shepherd.sharice@gmail.com>
2019-07-03 21:36:48 -0700

    amended copyright

--- asciilines.py
+++ asciilines.py
@@ -1,4 +1,4 @@
-# Copyright @ Sharice Mayer
+# Copyright @ Eliot Woodrich
 # [This program is licensed under the "MIT License"]
 # Please see the file LICENSE in the source
 # distribution of this software for license terms.
